e10cd865577ecd7e060f2a9d64d90a18152067f6
bytesparallel <jason.ross841@gmail.com>
2021-12-10 10:02:38 -0600

    saving changes so far

--- src/components/home/GraphicsBayTrackerHome.js
+++ src/components/home/GraphicsBayTrackerHome.js
@@ -1,8 +1,10 @@
+import React from "react"
 import { NavBar } from "../nav/NavBar"
 
 export const GraphicsBayTrackerHome =()=>{
-    
+    const displayNavBar = NavBar()
+
     return <div>
-        
+        {displayNavBar}
     </div>
 }
